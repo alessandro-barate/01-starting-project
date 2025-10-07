6341521b6471364fb8cde1088c94083142e1bef8
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2025-10-07 19:19:06 +0200

    Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,7 @@
-import reactImg from "./assets/react-core-concepts.png";
-import componentsImg from "./assets/components.png";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-const description = reactDescriptions[genRandomInt(2)];
-
-function Header() {
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+// import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data.js";
 
 function App() {
   return (
@@ -41,25 +12,15 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept
-              title="Components"
-              description="The core UI building block"
-              image={componentsImg}
+              title={CORE_CONCEPTS[0].title} // Posso fare anche così, quando le props non sono in un file separato -> title="Components" e image={componentsImg}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept
-              title="Props"
-              description="The core UI building block"
-              image={componentsImg}
-            />
-            <CoreConcept
-              title=""
-              description="The core UI building block"
-              image={componentsImg}
-            />
-            <CoreConcept
-              title="Components"
-              description="The core UI building block"
-              image={componentsImg}
-            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {/* Il metodo con lo spread operator (...) è decisamente più veloce. Lo
+            spread in pratica legge estrapola tutte le coppie chiave-valore dell'oggetto indicato */}
           </ul>
         </section>
       </main>
---
+++ src/components/CoreConcept.jsx
@@ -0,0 +1,21 @@
+// function CoreConcept(props) {
+//   return (
+//     <li>
+//       <img src={props.image} alt={props.title} />
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>
+//     </li>
+//   );
+// }
+// Questo sopra si può scrivere nel modo qui sotto:
+
+export default function CoreConcept({ image, description, title }) {
+  // Le parentesi graffe all'interno dell'area dei parametri, destrutturano l'oggetto props, dando accesso direttamente alla coppia chiave-valore
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
