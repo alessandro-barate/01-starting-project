a8336349a6241d6a6295c5c2af4c0dbaf978b973
alessandro-barate <alebarat@gmail.com>
2025-10-16 17:28:46 +0200

    Displaying conditional contents

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 // import componentsImg from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data.js";
+import { EXAMPLES } from "./data.js";
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
+  const [ selectedTopic, setSelectedTopic ] = useState();
 
   function handleClick(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -43,7 +44,52 @@ function App() {
             <TabButton onClick={() => handleClick('state')}>State</TabButton>
             {/* La arrow function serve per poter chiamare una funzione che deve passare alcuni argomenti. */}
           </menu>
-          {selectedTopic}
+          {!selectedTopic ? <p>Please select a topic.</p> : null}
+          {selectedTopic ? <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div> : null}
+
+          {/* Questo è un modo di scrivere lo stesso codice di sopra usando l'operatore &&
+          {!selectedTopic && <p>Please select a topic.</p>}
+          {selectedTopic && <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>} */}
+
+          {/* Questo è un modo di scrivere lo stesso codice di sopra ma più corto
+          {!selectedTopic ? <p>Please select a topic.</p> : (<div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>)} */}
+
+          {/* Questo è un modo di scrivere lo stesso codice di sopra usando però una variabile e una condizione if
+          let tabContent = <p>Please select a topic.</p>;
+          if (selectedTopic) {
+            tabContent = (<div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+          )}
+
+          return...
+          
+          ...</menu>
+          {tabContent}
+          </section>... */}
+
         </section>
       </main>
     </div>
