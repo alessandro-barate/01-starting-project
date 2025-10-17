686a25113579308a7f6a426e87a8c1b16ba11915
alessandro-barate <alebarat@gmail.com>
2025-10-17 23:10:23 +0200

    Outoutting dinamic contents

--- src/App.jsx
+++ src/App.jsx
@@ -22,14 +22,16 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
+            {/* Questa scrittura sopra sostituisce quella sotto, perchè così facendo gli elementi di CORE_CONCEPTS vengono generati dinamicamente. Sotto, invece, se per caso un elemento di CORE_CONCEPTS fosse commentato, in pagina avremmo uno spazio vuoto visibile */}
+            {/* <CoreConcept
               title={CORE_CONCEPTS[0].title} // Posso fare anche così, quando le props non sono in un file separato -> title="Components" e image={componentsImg}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
             {/* Il metodo con lo spread operator (...) è decisamente più veloce. Lo
             spread in pratica legge estrapola tutte le coppie chiave-valore dell'oggetto indicato */}
           </ul>
@@ -38,10 +40,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onClick={() => handleClick('components')}>Components</TabButton>
-            <TabButton onClick={() => handleClick('jsx')}>JSX</TabButton>
-            <TabButton onClick={() => handleClick('props')}>Props</TabButton>
-            <TabButton onClick={() => handleClick('state')}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleClick('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleClick('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleClick('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleClick('state')}>State</TabButton>
             {/* La arrow function serve per poter chiamare una funzione che deve passare alcuni argomenti. */}
           </menu>
           {!selectedTopic ? <p>Please select a topic.</p> : null}
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -8,10 +8,10 @@
 //}
 
 // Altro modo di scrivere il codice sopra, usando la destrutturazione
-export default function TabButton({children, onClick}) {
+export default function TabButton({children, onClick, isSelected}) {
   return (
     <li>
-      <button onClick={onClick}>{children}</button>
+      <button className={isSelected ? 'active' : undefined} onClick={onClick}>{children}</button>
     </li>
   );
 }
