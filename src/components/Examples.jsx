d7ca242a2e1169d343c910416ce9ff977d8f54ac
alessandro-barate <alebarat@gmail.com>
2025-10-21 19:21:27 +0200

    Creating Tabs component

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,70 +1,20 @@
 import TabButton from "./TabButton.jsx";
 import Section from "./Section.jsx";
+import Tabs from "./Tabs.jsx";
 import { EXAMPLES } from "../data.js";
 import { useState } from "react";
 
 export default function Examples() {
 
     const [ selectedTopic, setSelectedTopic ] = useState();
-    
-      function handleClick(selectedButton) {
-        setSelectedTopic(selectedButton);
-        // selectedButton -> 'components', 'jsx', 'props', 'state'
-        console.log(selectedTopic);
-      }
-
-    return (
-        <Section title="Examples" id="examples">
-          <menu>
-            {/* First button */}
-            <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleClick('components')}>Components</TabButton>
-            {/* END first button */}
-
-            {/* Second button */}
-            <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleClick('jsx')}>JSX</TabButton>
-            {/* END second button */}
-
-            {/* Third button */}
-            <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleClick('props')}>Props</TabButton>
-            {/* END third button */}
             
-            {/* Fourth button */}
-            <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleClick('state')}>State</TabButton>
-            {/* END fourth button */}
+              function handleClick(selectedButton) {
+                setSelectedTopic(selectedButton);
+                // selectedButton -> 'components', 'jsx', 'props', 'state'
+                console.log(selectedTopic);
+              }
 
-            {/* La arrow function serve per poter chiamare una funzione che deve passare alcuni argomenti. */}
-
-          </menu>
-          {!selectedTopic ? <p>Please select a topic.</p> : null}
-          {selectedTopic ? <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div> : null}
-
-          {/* Questo è un modo di scrivere lo stesso codice di sopra usando l'operatore &&
-          {!selectedTopic && <p>Please select a topic.</p>}
-          {selectedTopic && <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>} */}
-
-          {/* Questo è un modo di scrivere lo stesso codice di sopra ma più corto
-          {!selectedTopic ? <p>Please select a topic.</p> : (<div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>)} */}
-
-          {/* Questo è un modo di scrivere lo stesso codice di sopra usando però una variabile e una condizione if
-          let tabContent = <p>Please select a topic.</p>;
+    let tabContent = <p>Please select a topic.</p>;
           if (selectedTopic) {
             tabContent = (<div id="tab-content">
             <h3>{EXAMPLES[selectedTopic].title}</h3>
@@ -75,12 +25,63 @@ export default function Examples() {
           </div>
           )}
 
-          return...
-          
-          ...</menu>
-          {tabContent}
-          </section>... */}
-
+    return (
+        <Section title="Examples" id="examples">
+          <Tabs 
+            buttons={<>
+                {/* First button */}
+                <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleClick('components')}>Components</TabButton>
+                {/* END first button */}
+            
+                {/* Second button */}
+                <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleClick('jsx')}>JSX</TabButton>
+                {/* END second button */}
+            
+                {/* Third button */}
+                <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleClick('props')}>Props</TabButton>
+                {/* END third button */}
+                        
+                {/* Fourth button */}
+                <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleClick('state')}>State</TabButton>
+                {/* END fourth button */}
+            </>
+                
+          }>
+            {tabContent}
+          </Tabs>
         </Section>
-    )
+          
+        
+        //   Questo è un modo di scrivere lo stesso codice di sopra usando l'operatore &&
+        //   {!selectedTopic && <p>Please select a topic.</p>}
+        //   {selectedTopic && <div id="tab-content">
+        //     <h3>{EXAMPLES[selectedTopic].title}</h3>
+        //     <p>{EXAMPLES[selectedTopic].description}</p>
+        //     <pre>
+        //       <code>{EXAMPLES[selectedTopic].code}</code>
+        //     </pre>
+        //   </div>}
+
+        //   Questo è un modo di scrivere lo stesso codice di sopra ma più corto
+        //   {!selectedTopic ? <p>Please select a topic.</p> : (<div        id="tab-content">
+        //     <h3>{EXAMPLES[selectedTopic].title}</h3>
+        //     <p>{EXAMPLES[selectedTopic].description}</p>
+        //     <pre>
+        //       <code>{EXAMPLES[selectedTopic].code}</code>
+        //     </pre>
+        //   </div>)}
+
+        //   {/* Questo è un modo di scrivere lo stesso codice di sopra usando però una variabile e una condizione if
+
+        //     {!selectedTopic ? <p>Please select a topic.</p> : null}
+        //   {selectedTopic ? <div id="tab-content">
+        //     <h3>{EXAMPLES[selectedTopic].title}</h3>
+        //     <p>{EXAMPLES[selectedTopic].description}</p>
+        //     <pre>
+        //       <code>{EXAMPLES[selectedTopic].code}</code>
+        //     </pre>
+        //   </div> : null}
+
+        
+    );
 }
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -8,10 +8,10 @@
 //}
 
 // Altro modo di scrivere il codice sopra, usando la destrutturazione
-export default function TabButton({children, onClick, isSelected}) {
+export default function TabButton({children, isSelected, ...props}) {
   return (
     <li>
-      <button className={isSelected ? 'active' : undefined} onClick={onClick}>{children}</button>
+      <button className={isSelected ? 'active' : undefined} {...props}>{children}</button>
     </li>
   );
 }
